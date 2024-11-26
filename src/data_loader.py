e257530d4f559ce0d6995b7827e2d045945ada9a
ZS912719 <shengxun.zhao@gmail.com>
2024-11-26 23:03:14 +0100

    update data exploration files

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+TRAIN_DATA_PATH = "../data/raw/train.csv"
+TEST_DATA_PATH = "../data/raw/test.csv"
 
-DATA_PATH = "../data/raw/train.csv"
-load_data = pd.read_csv(DATA_PATH)
+df_train = pd.read_csv(TRAIN_DATA_PATH)
+df_test = pd.read_csv(TEST_DATA_PATH)
+
