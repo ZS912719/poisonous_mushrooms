0e449988302bb31ee3f636d424675b24e3ff5344
ZS912719 <shengxun.zhao@gmail.com>
2024-12-05 16:02:14 +0100

    rectify one-hot encoder

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,19 +1,17 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+from sklearn import preprocessing
+
 
 class DataSet:
 
-    def __init__(self, df:pd.DataFrame, seed:int):
+    def __init__(self, df:pd.DataFrame):
         self.df = df
-        self.rng = np.random.default_rng(seed)
-        self.df_processed = None
         self.df_sample = None
+        self.X = None
 
-    def __len__(self):
-        return len(self.df_sample)
-
-    def preprocess(self):
+    @staticmethod
+    def preprocess(df:pd.DataFrame):
         """
         Preprocess the dataset:
         1. Remove columns with more than 60% missing values.
@@ -21,8 +19,6 @@ class DataSet:
             - With mode for categorical columns.
             - With median for numerical columns.
         """
-        df = self.df.copy()
-
         threshold = 0.6
 
         high_missing_cols = df.columns[df.isnull().mean() > threshold]
@@ -35,54 +31,45 @@ class DataSet:
                 else:
                     df_processed[col] = df_processed[col].fillna(df_processed[col].median())
 
-        self.df_processed = df_processed
+        return df_processed
 
-        return self
-
-    def get_sample(self):
+    @staticmethod
+    def get_sample(df:pd.DataFrame):
         """
         take out 500,000 samples randomly to do training
         """
-        df = self.df_processed
-        df_sample = df.sample(n=500000, random_state=self.rng)
-        self.df_sample = df_sample
+        df_sample = df.sample(n=50, random_state= 0)
+        return df_sample
 
-    def split(self):
-        sample = self.df_sample.copy().drop("class", axis=1)
-        numerical_columns = sample.select_dtypes(include=["int64", "float64"]).columns
-        categorical_columns = sample.select_dtypes(exclude=["int64", "float64"]).columns
+    @staticmethod
+    def split_features(sample):
+        sample = sample.drop(["id","class"],axis=1)
+        numerical_columns = sample.select_dtypes(include=["int64", "float64"])
+        categorical_columns = sample.select_dtypes(exclude=["int64", "float64"])
         return numerical_columns, categorical_columns
+    
+    def encode(self):
+        sample = self.df_sample
+        # y = sample["class"]
+        numerical_columns, categorical_columns = self.split_features(sample)
+        enc_onehot = preprocessing.OneHotEncoder()
+
+        # X_num = ['test']
+        X_cat = []
+        
+        enc_onehot.fit(categorical_columns)
+        onehot_array = enc_onehot.transform(categorical_columns).toarray()
+        X_cat.append(onehot_array)
+        self.X =  X_cat
+
 
     def __call__(self):
-        self.preprocess()
-        self.get_sample()
+        df_processed = self.preprocess(self.df.copy())
+        self.df_sample = self.get_sample(df_processed)
+        self.encode()
         print("subset made and encoded")
 
+    
     def __getitem__(self, idx):
-
-        sample = self.df_sample.iloc[idx]
-        part_X = sample.drop("class")
-        part_y = sample["class"]
-
-        numerical_columns, categorical_columns = self.split()
-
-        X_num = []
-        if len(numerical_columns) > 0:
-            scaler = MinMaxScaler()
-            scaler.fit(self.df_sample[numerical_columns])
-            X_num = scaler.transform(part_X[numerical_columns].values.reshape(1, -1)).flatten()
-
-        X_cat = []
-        for col in categorical_columns:
-            unique_values = self.df_sample[col].unique()
-            unique_map = {v: i for i, v in enumerate(unique_values)}
-            onehot_array = np.zeros(len(unique_values), float)
-            onehot_index = unique_map[part_X[col]]
-            onehot_array[onehot_index] = 1
-            X_cat.append(onehot_array)
-
-        X = np.concatenate([X_num] + X_cat)
-
-        y = 0 if part_y == 'e' else 1
-
-        return X, y
+        X = self.X[idx]
+        return X
