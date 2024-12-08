0939b9d2b470559e751e465a047010dc8e25463b
phianke <shengxun.zhao@gmail.com>
2024-12-08 22:08:08 +0100

    rename dataframes

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,6 +3,6 @@ import pandas as pd
 TRAIN_DATA_PATH = "../data/raw/train.csv"
 TEST_DATA_PATH = "../data/raw/test.csv"
 
-df_train = pd.read_csv(TRAIN_DATA_PATH)
-df_test = pd.read_csv(TEST_DATA_PATH)
+train_set = pd.read_csv(TRAIN_DATA_PATH)
+test_set = pd.read_csv(TEST_DATA_PATH)
 
