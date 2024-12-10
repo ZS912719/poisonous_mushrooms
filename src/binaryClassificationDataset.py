bc57b79293f5f016d86b34cab4c11859c13562f9
ZS912719 <shengxun.zhao@gmail.com>
2024-12-10 15:36:25 +0100

    complete binaryClassificationDataset.py: redefine y_batch type

--- src/binaryClassificationDataset.py
+++ src/binaryClassificationDataset.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import torch
 
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 
 class BinaryClassificationDataset(Dataset):
-    def __init__(self, data, df: pd.DataFrame):
+    def __init__(self, data):
         """
         Args:
             data: List of tuples, where each tuple is (features, label).
         """
-        super().__init__(df)
         self.data = data
 
     def __len__(self):
@@ -18,4 +17,7 @@ class BinaryClassificationDataset(Dataset):
 
     def __getitem__(self, idx):
         features, label = self.data[idx]
-        return torch.tensor(features, dtype=torch.float32), torch.tensor(label, dtype=torch.long)
+        return (
+            torch.tensor(features, dtype=torch.float32),
+            torch.tensor(label, dtype=torch.float32)
+        )
